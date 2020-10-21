706a638fd416c0e4f3b8f064024aed7dadb22156
Luke Iverson <lukeiverson@Lukes-MacBook-Pro.local>
2020-10-21 15:28:44 -0500

    Objects/Loops/Functions

---
+++ iphone12.js
@@ -0,0 +1,12 @@
+const iphone12 = {
+    
+    image: "path to where the image is",
+    length: "11.28",
+    title: "iPhone 12 unboxing experience + Magsafe",
+    username: "Marques brownlee",
+    views: "5.5M",
+    uploaded: "15",
+
+}
+
+console.log(iphone12.title)
---
+++ starwars.js
@@ -0,0 +1,11 @@
+const StarWars = {
+
+image: "Path to where image is located",
+title: "Special Look | The Mandalorian",
+views: "1M",
+username: "Star Wars",
+uploaded: "1",
+
+}
+
+console.log(StarWars.title)
